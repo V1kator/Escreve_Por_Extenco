d6240959a46313481f918543254927d0cb34b7d5
Victor Daniel <Victor.34241361@gmail.com>
2023-03-30 14:54:09 -0300

    Função Conferesetemletras() adicionada

--- Escreve_Por_Extenço/Program.cs
+++ Escreve_Por_Extenço/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿Console.WriteLine("Informe alguns Número (somente inteiros):");
+string texto = Console.ReadLine();
+
+List<char> listadecaracteres = new List<char>();
+List<string> listadecaracterespermitidos = new List<string>();
+bool contemletras = false;
+
+Conferesetemletras(ref texto, ref contemletras, ref listadecaracteres, ref listadecaracterespermitidos);
+
+if(contemletras)
+{
+    Console.WriteLine("Erro: o texto informado contém letras ou caracteres inválidos.");
+}
+else
+{
+   
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+static void Conferesetemletras(ref string texto, ref bool contemletras, ref List<char> listadecaracteres, ref List<string> listadecaracterespermitidos)
+{
+
+    foreach (char c in texto)
+    {
+        listadecaracteres.Add(c);
+    }
+
+    for (int i = 0; i <= 9; i++)
+    {
+        string caracterespermitidos = Convert.ToString(i);
+        listadecaracterespermitidos.Add(caracterespermitidos);
+    }
+
+    foreach (char c in listadecaracteres)
+    {
+        string s = Convert.ToString(c);
+        if (!listadecaracterespermitidos.Contains(s))
+        {
+            contemletras = true;
+            break;
+        }
+    }
+}
